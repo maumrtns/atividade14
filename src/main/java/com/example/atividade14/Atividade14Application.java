52cd21d98e774cfd369470ae1207f2c120b42bd6
Maurício <mauro.cavalcante18@gmail.com>
2024-05-31 20:21:27 -0300

    Atividade14

---
+++ src/main/java/com/example/atividade14/Atividade14Application.java
@@ -0,0 +1,13 @@
+package com.example.atividade14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atividade14/Atividade14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividade14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
